59363bda585372c6af6f1f8322a34826342ea6dd
Егор Артемов <me@badsysadm.com>
2023-12-10 20:48:35 +0300

    Update env.sh

--- LimeSurvey/env.sh
+++ LimeSurvey/env.sh
@@ -8,8 +8,9 @@ LIMESURVEY_DB_NAME=${LIMESURVEY_DB_NAME:-limesurvey}
 LIMESURVEY_TEMPLATE='/var/lib/limesurvey/config.php'
 LIMESURVEY_CONFIGFILE='/var/www/limesurvey/application/config/config.php'
 
-cat "${LIMESURVEY_TEMPLATE}" | sed -s  's/LIMESURVEY_DB_USERNAME/'"${LIMESURVEY_DB_USERNAME}"'/g' > "${LIMESURVEY_CONFIGFILE}"
-cat "${LIMESURVEY_TEMPLATE}" | sed -s  's/LIMESURVEY_DB_PASSWORD/'"${LIMESURVEY_DB_PASSWORD}"'/g' > "${LIMESURVEY_CONFIGFILE}"
-cat "${LIMESURVEY_TEMPLATE}" | sed -s  's/LIMESURVEY_DB_HOST/'"${LIMESURVEY_DB_HOST}"'/g' > "${LIMESURVEY_CONFIGFILE}"
-cat "${LIMESURVEY_TEMPLATE}" | sed -s  's/LIMESURVEY_DB_PORT/'"${LIMESURVEY_DB_PORT}"'/g' > "${LIMESURVEY_CONFIGFILE}"
-cat "${LIMESURVEY_TEMPLATE}" | sed -s  's/LIMESURVEY_DB_NAME/'"${LIMESURVEY_DB_NAME}"'/g' > "${LIMESURVEY_CONFIGFILE}"
+cat "${LIMESURVEY_TEMPLATE}" | sed -s  's/LIMESURVEY_DB_USERNAME/'"${LIMESURVEY_DB_USERNAME}"'/g' \
+                             | sed -s  's/LIMESURVEY_DB_PASSWORD/'"${LIMESURVEY_DB_PASSWORD}"'/g' \
+                             | sed -s  's/LIMESURVEY_DB_HOST/'"${LIMESURVEY_DB_HOST}"'/g' \
+                             | sed -s  's/LIMESURVEY_DB_PORT/'"${LIMESURVEY_DB_PORT}"'/g' \
+                             | sed -s  's/LIMESURVEY_DB_NAME/'"${LIMESURVEY_DB_NAME}"'/g' \
+                             > "${LIMESURVEY_CONFIGFILE}"
